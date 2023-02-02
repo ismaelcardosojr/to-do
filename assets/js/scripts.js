767310e148a4a73f2e1e27b8af99ea2900d0293b
Ismael <112780968+ismaelcardosojr@users.noreply.github.com>
2023-02-02 18:55:58 -0300

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,57 @@
+function addTask() {
+    const taskInp = document.querySelector("#task-input");
+    const taskTitle = taskInp.value;
+    
+    taskInp.value = "";
+
+    if (!taskTitle) {
+        return;
+    }
+
+    const newTask = document.querySelector(".template").cloneNode(true);
+
+    newTask.querySelector(".task-title").textContent = taskTitle;
+    newTask.classList.remove("template");
+    newTask.classList.remove("hide");
+
+    appendCompleteEvent(newTask);
+    appendDescartEvent(newTask);
+
+    document.querySelector("#task-list").appendChild(newTask);
+}
+
+function appendCompleteEvent(newTask) {
+    const completeBtn = newTask.querySelector(".complete-button");
+    let counter = 1;
+
+    completeBtn.onclick = () => {
+        const taskTitle = newTask.querySelector(".task-title");
+
+        if (counter % 2 != 0) {
+            taskTitle.style.cssText = "text-decoration: line-through; color: var(--prominent-color)";
+        } else {
+            taskTitle.style.cssText = "text-decoration: none; color: var(--trans-black-color)";
+        }
+
+        counter++;
+        console.log(counter);
+    }
+}
+
+function appendDescartEvent(newTask) {
+    const descartBtn = newTask.querySelector(".descart-button");
+
+    descartBtn.onclick = () => {
+        const tasksList = newTask.parentNode;
+        tasksList.removeChild(newTask);
+    }
+}
+
+/* --- */
+
+const addBtn = document.querySelector(".add-button");
+
+addBtn.onclick = e => {
+    e.preventDefault();
+    addTask();
+}
